0aba440ddf35987eb4f2687a99b25de5e9779709
Erkebulan-Akhmediya <yerkebulan20030921@gmail.com>
2025-04-03 23:15:51 +0500

    updated fighter entity

--- src/fighter/fighter.entity.ts
+++ src/fighter/fighter.entity.ts
@@ -5,12 +5,24 @@ export class Fighter {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ name: 'last_name', type: 'varchar', length: 255 })
-  lastName: string;
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
 
-  @Column({ name: 'first_name', type: 'varchar', length: 255 })
-  firstName: string;
+  @Column({ type: 'varchar', length: 255 })
+  team: string;
 
-  @Column({ name: 'middle_name', type: 'varchar', length: 255 })
-  middleName: string;
+  @Column({ type: 'varchar', length: 255 })
+  nationality: string;
+
+  @Column({ name: 'birth_datae' })
+  birthDate: Date;
+
+  @Column({ name: 'birth_place', type: 'varchar', length: 255 })
+  birthPlace: string;
+
+  @Column({ name: 'weight' })
+  weight: number;
+
+  @Column({ name: 'nickname' })
+  nickname: string;
 }
